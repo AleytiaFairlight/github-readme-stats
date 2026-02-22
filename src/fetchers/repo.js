3cf1c2f2826e15d9b6e27f1b8840ceaa07b313f4
Aleytia Fairlight <scarletdevil25@gmail.com>
2026-02-22 10:54:24 +0800

    Update repo.js
    I'm stupid so this has to be modified again

--- src/fetchers/repo.js
+++ src/fetchers/repo.js
@@ -1,276 +1,119 @@
 // @ts-check
-
-import axios from "axios";
-import * as dotenv from "dotenv";
-import githubUsernameRegex from "github-username-regex";
-import { calculateRank } from "../calculateRank.js";
 import { retryer } from "../common/retryer.js";
-import { logger } from "../common/log.js";
-import { excludeRepositories } from "../common/envs.js";
-import { CustomError, MissingParamError } from "../common/error.js";
-import { wrapTextMultiline } from "../common/fmt.js";
-import { request } from "../common/http.js";
-
-dotenv.config();
-
-// GraphQL queries.
-// FIX: Added privacy: ALL to include private repositories
-const GRAPHQL_REPOS_FIELD = `
-  repositories(first: 100, ownerAffiliations: OWNER, privacy: ALL, orderBy: {direction: DESC, field: STARGAZERS}, after: $after) {
-    totalCount
-    nodes {
-      name
-      stargazers {
-        totalCount
-      }
-    }
-    pageInfo {
-      hasNextPage
-      endCursor
-    }
-  }
-`;
-
-const GRAPHQL_REPOS_QUERY = `
-  query userInfo($login: String!, $after: String) {
-    user(login: $login) {
-      ${GRAPHQL_REPOS_FIELD}
-    }
-  }
-`;
-
-const GRAPHQL_STATS_QUERY = `
-  query userInfo($login: String!, $after: String, $includeMergedPullRequests: Boolean!, $includeDiscussions: Boolean!, $includeDiscussionsAnswers: Boolean!, $startTime: DateTime = null) {
-    user(login: $login) {
-      name
-      login
-      commits: contributionsCollection (from: $startTime) {
-        totalCommitContributions,
-      }
-      reviews: contributionsCollection {
-        totalPullRequestReviewContributions
-      }
-      repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {
-        totalCount
-      }
-      pullRequests(first: 1) {
-        totalCount
-      }
-      mergedPullRequests: pullRequests(states: MERGED) @include(if: $includeMergedPullRequests) {
-        totalCount
-      }
-      openIssues: issues(states: OPEN) {
-        totalCount
-      }
-      closedIssues: issues(states: CLOSED) {
-        totalCount
-      }
-      followers {
-        totalCount
-      }
-      repositoryDiscussions @include(if: $includeDiscussions) {
-        totalCount
-      }
-      repositoryDiscussionComments(onlyAnswers: true) @include(if: $includeDiscussionsAnswers) {
-        totalCount
-      }
-      ${GRAPHQL_REPOS_FIELD}
-    }
-  }
-`;
+import { MissingParamError, request } from "../common/utils.js";
 
 /**
- * Stats fetcher object.
+ * @typedef {import('axios').AxiosRequestHeaders} AxiosRequestHeaders Axios request headers.
+ * @typedef {import('axios').AxiosResponse} AxiosResponse Axios response.
+ */
+
+/**
+ * Repo data fetcher.
  *
- * @param {object & { after: string | null }} variables Fetcher variables.
+ * @param {AxiosRequestHeaders} variables Fetcher variables.
  * @param {string} token GitHub token.
- * @returns {Promise<import('axios').AxiosResponse>} Axios response.
+ * @returns {Promise<AxiosResponse>} The response.
  */
 const fetcher = (variables, token) => {
-  const query = variables.after ? GRAPHQL_REPOS_QUERY : GRAPHQL_STATS_QUERY;
   return request(
     {
-      query,
+      query: `
+      fragment RepoInfo on Repository {
+        name
+        nameWithOwner
+        isPrivate
+        isArchived
+        isTemplate
+        stargazers {
+          totalCount
+        }
+        description
+        primaryLanguage {
+          color
+          id
+          name
+        }
+        forkCount
+      }
+      query getRepo($login: String!, $repo: String!) {
+        user(login: $login) {
+          repository(name: $repo) {
+            ...RepoInfo
+          }
+        }
+        organization(login: $login) {
+          repository(name: $repo) {
+            ...RepoInfo
+          }
+        }
+      }
+    `,
       variables,
     },
     {
-      Authorization: `bearer ${token}`,
+      Authorization: `token ${token}`,
     },
   );
 };
 
+const urlExample = "/api/pin?username=USERNAME&amp;repo=REPO_NAME";
+
 /**
- * Fetch stats information for a given username.
- *
- * @param {object} variables Fetcher variables.
- * @param {string} variables.username GitHub username.
- * @param {boolean} variables.includeMergedPullRequests Include merged pull requests.
- * @param {boolean} variables.includeDiscussions Include discussions.
- * @param {boolean} variables.includeDiscussionsAnswers Include discussions answers.
- * @param {string|undefined} variables.startTime Time to start the count of total commits.
- * @returns {Promise<import('axios').AxiosResponse>} Axios response.
- *
- * @description This function supports multi-page fetching if the 'FETCH_MULTI_PAGE_STARS' environment variable is set to true.
+ * @typedef {import("./types").RepositoryData} RepositoryData Repository data.
  */
-const statsFetcher = async ({
-  username,
-  includeMergedPullRequests,
-  includeDiscussions,
-  includeDiscussionsAnswers,
-  startTime,
-}) => {
-  let stats;
-  let hasNextPage = true;
-  let endCursor = null;
-  while (hasNextPage) {
-    const variables = {
-      login: username,
-      first: 100,
-      after: endCursor,
-      includeMergedPullRequests,
-      includeDiscussions,
-      includeDiscussionsAnswers,
-      startTime,
-    };
-    let res = await retryer(fetcher, variables);
-    if (res.data.errors) {
-      return res;
-    }
-
-    // Store stats data.
-    const repoNodes = res.data.data.user.repositories.nodes;
-    if (stats) {
-      stats.data.data.user.repositories.nodes.push(...repoNodes);
-    } else {
-      stats = res;
-    }
-
-    // Disable multi page fetching on public Vercel instance due to rate limits.
-    const repoNodesWithStars = repoNodes.filter(
-      (node) => node.stargazers.totalCount !== 0,
-    );
-    hasNextPage =
-      process.env.FETCH_MULTI_PAGE_STARS === "true" &&
-      repoNodes.length === repoNodesWithStars.length &&
-      res.data.data.user.repositories.pageInfo.hasNextPage;
-    endCursor = res.data.data.user.repositories.pageInfo.endCursor;
-  }
-
-  return stats;
-};
 
 /**
- * Fetch total commits using the REST API.
- *
- * @param {object} variables Fetcher variables.
- * @param {string} token GitHub token.
- * @returns {Promise<import('axios').AxiosResponse>} Axios response.
- *
- * @see https://developer.github.com
- */
-const fetchTotalCommits = (variables, token) => {
-  return axios({
-    method: "get",
-    url: `https://api.github.com{variables.login}`,
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/vnd.github.cloak-preview",
-      Authorization: `token ${token}`,
-    },
-  });
-};
-
-/**
- * Fetch all the commits for all the repositories of a given username.
+ * Fetch repository data.
  *
  * @param {string} username GitHub username.
- * @returns {Promise<number>} Total commits.
+ * @param {string} reponame GitHub repository name.
+ * @returns {Promise<RepositoryData>} Repository data.
  */
-const totalCommitsFetcher = async (username) => {
-  if (!githubUsernameRegex.test(username)) {
-    logger.log("Invalid username provided.");
-    throw new Error("Invalid username provided.");
+const fetchRepo = async (username, reponame) => {
+  if (!username && !reponame) {
+    throw new MissingParamError(["username", "repo"], urlExample);
   }
-
-  let res;
-  try {
-    res = await retryer(fetchTotalCommits, { login: username });
-  } catch (err) {
-    logger.log(err);
-    throw new Error(err);
-  }
-
-  const totalCount = res.data.total_count;
-  if (!totalCount || isNaN(totalCount)) {
-    throw new CustomError(
-      "Could not fetch total commits.",
-      CustomError.GITHUB_REST_API_ERROR,
-    );
-  }
-  return totalCount;
-};
-
-/**
- * Fetch stats for a given username.
- */
-const fetchStats = async (
-  username,
-  include_all_commits = false,
-  exclude_repo = [],
-  include_merged_pull_requests = false,
-  include_discussions = false,
-  include_discussions_answers = false,
-  commits_year,
-) => {
   if (!username) {
-    throw new MissingParamError(["username"]);
+    throw new MissingParamError(["username"], urlExample);
+  }
+  if (!reponame) {
+    throw new MissingParamError(["repo"], urlExample);
   }
 
-  const stats = {
-    name: "",
-    totalPRs: 0,
-    totalPRsMerged: 0,
-    mergedPRsPercentage: 0,
-    totalReviews: 0,
-    totalCommits: 0,
-    totalIssues: 0,
-    totalStars: 0,
-    totalDiscussionsStarted: 0,
-    totalDiscussionsAnswered: 0,
-    contributedTo: 0,
-    rank: { level: "C", percentile: 100 },
-  };
+  let res = await retryer(fetcher, { login: username, repo: reponame });
 
-  let res = await statsFetcher({
-    username,
-    includeMergedPullRequests: include_merged_pull_requests,
-    includeDiscussions: include_discussions,
-    includeDiscussionsAnswers: include_discussions_answers,
-    startTime: commits_year ? `${commits_year}-01-01T00:00:00Z` : undefined,
-  });
+  const data = res.data.data;
 
-  // Catch GraphQL errors.
-  if (res.data.errors) {
-    logger.error(res.data.errors);
-    if (res.data.errors[0].type === "NOT_FOUND") {
-      throw new CustomError(
-        res.data.errors[0].message || "Could not fetch user.",
-        CustomError.USER_NOT_FOUND,
-      );
-    }
-    if (res.data.errors[0].message) {
-      throw new CustomError(
-        wrapTextMultiline(res.data.errors[0].message, 90, 1)[0],
-        res.statusText,
-      );
-    }
-    throw new CustomError(
-      "Something went wrong while trying to retrieve the stats data using the GraphQL API.",
-      res.statusText,
-    );
+  if (!data.user && !data.organization) {
+    throw new Error("Not found");
   }
 
-  // Rest of processing logic...
-  // (Assuming your original logic continues here to populate the `stats` object)
-  return stats;
+  const isUser = data.organization === null && data.user;
+  const isOrg = data.user === null && data.organization;
+
+  if (isUser) {
+    if (!data.user.repository || data.user.repository.isPrivate) {
+      throw new Error("User Repository Not found");
+    }
+    return {
+      ...data.user.repository,
+      starCount: data.user.repository.stargazers.totalCount,
+    };
+  }
+
+  if (isOrg) {
+    if (
+      !data.organization.repository /* || data.organization.repository.isPrivate*/) {
+      throw new Error("Organization Repository Not found");
+    }
+    return {
+      ...data.organization.repository,
+      starCount: data.organization.repository.stargazers.totalCount,
+    };
+  }
+
+  throw new Error("Unexpected behavior");
 };
+
+export { fetchRepo };
+export default fetchRepo;
